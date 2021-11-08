4fd92a6489c89ee35674490aa89e92d05af4fc5f
Joseph Fulkerson <josephafulkerson@gmail.com>
2021-11-08 10:39:00 -0500

    commit tree 7c5214891e4e52f3498d02475bc7645da3882a3a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,51 @@
 class ItemsController < ApplicationController
 
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      users = User.find(params[:user_id])
+      items = users.items
+    else
+      items = Item.all
+    end
+    render json: items
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create 
+    item = Item.create(permit_items)
+    # byebug
+    render json: item, status: 201
+  end
+  private
+
+  def render_not_found
+    render json: {error: 'user not found'}, status: 404
+  end
+
+  def permit_items
+    params.permit(:name, :description, :price, :user_id)
+
   end
 
 end
+
+
+
+# def index
+#   if params[:dog_house_id]
+#     dog_house = DogHouse.find(params[:dog_house_id])
+#     reviews = dog_house.reviews
+#   else
+#     reviews = Review.all
+#   end
+#   render json: reviews, include: :dog_house
+#   end
+#   reviews = Review.all
+#   render json: reviews, include: :dog_house
+# end
